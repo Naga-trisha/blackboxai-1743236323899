d6a965aebe4e6a06e44ffd45c753e68b4acb718c
Naga-trisha <trishapagadala18@gmail.com>
2025-03-29 13:49:33 +0530

    Add app/src/main/java/com/example/myapp/MyApplication.kt 1743236344059

---
+++ app/src/main/java/com/example/myapp/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.example.myapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize any application-wide dependencies here
+    }
+}
